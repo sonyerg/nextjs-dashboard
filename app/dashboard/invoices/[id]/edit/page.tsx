a81743766c5541378fefc0a0dc994bf98c4c5723
sonyerg <ericksonrguinto@gmail.com>
2024-02-05 16:06:44 +1300

    added updateInvoice server action

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,31 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  // In addition to searchParams, page components also accept a prop called params which you can use to access the id.
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  // Notice how it's similar to your /create invoice page, except it imports a different form (from the edit-form.tsx
+  // file). This form should be pre-populated with a defaultValue for the customer's name, invoice amount, and status.
+  // To pre-populate the form fields, you need to fetch the specific invoice using id.
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
